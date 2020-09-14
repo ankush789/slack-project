9d2f006305561932ecab2334853b0720df56d521
ankush789 <ankushsharma.4771@gmail.com>
2020-09-14 21:32:53 +0530

    ADD ROUTING

---
+++ src/component/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import { SignIn } from "./";
+
+function Home() {
+  return <div>HOMe</div>;
+}
+function Some() {
+  return <div>SOME</div>;
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={SignIn} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/some" component={Some} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
